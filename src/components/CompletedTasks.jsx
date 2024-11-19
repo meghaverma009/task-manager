b238b9736d839b78a9cafb2aa9a469d8c7d4774f
meghaverma009 <meghaverma1884@gmail.com>
2024-11-19 21:25:34 +0530

    fix errors

--- src/components/CompletedTasks.jsx
+++ src/components/CompletedTasks.jsx
@@ -42,7 +42,6 @@
 // }
 // export default CompletedTasks;
 
-
 function CompletedTasks({ tasks = [], onToggleComplete, onDelete }) {
   console.log("Completed tasks", tasks);
   const completedTasks = tasks.filter((task) => task && task.completed);
@@ -53,33 +52,37 @@ function CompletedTasks({ tasks = [], onToggleComplete, onDelete }) {
         <p> No completed tasks yet. </p>
       ) : (
         <ul className="completedTasksList">
-          {completedTasks.map((task) => (
-            <li key={task.id} className="completedTaskCard">
-              <div className="taskDetails">
-                <h2 className="taskTitle">{task.title || "Untitled Task"}</h2>
-                <p className="taskDescription">
-                  {task.description || "No description available"}
-                </p>
-                <p className="taskDueDate">
-                  Completed on: {task.dueDate ? task.dueDate : "N/A"}
-                </p>
-              </div>
-              <div className="taskActions">
-                <button
-                  className="taskButton undoButton"
-                  onClick={() => onToggleComplete(task.id)}
-                >
-                  Undo
-                </button>
-                <button
-                  className="taskButton deleteButton"
-                  onClick={() => onDelete(task.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          ))}
+          {completedTasks.map((task) => {
+            console.log("task", task);
+
+            return (
+              <li key={task.id} className="completedTaskCard">
+                <div className="taskDetails">
+                  <h2 className="taskTitle">{task.title || "Untitled Task"}</h2>
+                  <p className="taskDescription">
+                    {task.description || "No description available"}
+                  </p>
+                  <p className="taskDueDate">
+                    Completed on: {task.dueDate ? task.dueDate : "N/A"}
+                  </p>
+                </div>
+                <div className="taskActions">
+                  <button
+                    className="taskButton undoButton"
+                    onClick={() => onToggleComplete(task?.id)}
+                  >
+                    Undo
+                  </button>
+                  <button
+                    className="taskButton deleteButton"
+                    onClick={() => onDelete(task.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
